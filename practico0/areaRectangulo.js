5f8f1ebf6dd300a13283a2c11bcaa80293563f21
Estefy <estefy.luz.tarasiuk.com>
2023-05-20 12:20:07 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,11 @@
+//inciso a
+let base = 5;
+let altura = 3;
+let area = base * altura;
+console.log(area);
+
+// inciso b - aca no se deben definir las variables nuevamente
+base = 12.5;
+altura = 5.33;
+area = base * altura;
+console.log(area);
---
+++ practico0/promedio3.js
@@ -0,0 +1,26 @@
+let num1;
+let num2;
+let num3;
+let promedio3;
+
+//inciso a
+num1 = 5;
+num2 = 7;
+num3 = 9;
+promedio3 = (num1 * num2 * num3)/3;
+console.log(promedio3);
+
+//inciso b
+num1 = -2;
+num2 = -3;
+num3 = -8;
+promedio3 = (num1 * num2 * num3)/3;
+console.log(promedio3);
+
+//inciso 3
+
+num1 = -1.2;
+num2 = 5.3;
+num3 = -3.3;
+promedio3 = (num1 * num2 * num3)/3;
+console.log(promedio3);
