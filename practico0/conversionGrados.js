9a2d299c3b3b8ea156762c25c5121708c1b5a9a4
Estefy <estefy.luz.tarasiuk.com>
2023-05-20 12:13:13 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,15 @@
+let fahrenheit;
+let celsius;
+
+//inciso a
+
+fahrenheit = 50;
+celsius = ((fahrenheit - 32) * (5 / 9));
+console.log(celsius);
+
+
+// inciso 2
+
+fahrenheit = 75;
+celsius = ((fahrenheit - 32) * (5 / 9));
+console.log(celsius);
