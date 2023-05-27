fa3fe79412c6b69ae566b68c2ad4af935689c237
Estefy <estefy.luz.tarasiuk.com>
2023-05-27 10:23:00 -0300

    corregi un error en el promedio

--- practico0/promedio3.js
+++ practico0/promedio3.js
@@ -7,14 +7,14 @@ let promedio3;
 num1 = 5;
 num2 = 7;
 num3 = 9;
-promedio3 = (num1 * num2 * num3)/3;
+promedio3 = (num1 + num2 + num3)/3;
 console.log(promedio3);
 
 //inciso b
 num1 = -2;
 num2 = -3;
 num3 = -8;
-promedio3 = (num1 * num2 * num3)/3;
+promedio3 = (num1 + num2 + num3)/3;
 console.log(promedio3);
 
 //inciso 3
@@ -22,5 +22,5 @@ console.log(promedio3);
 num1 = -1.2;
 num2 = 5.3;
 num3 = -3.3;
-promedio3 = (num1 * num2 * num3)/3;
+promedio3 = (num1 + num2 + num3)/3;
 console.log(promedio3);
