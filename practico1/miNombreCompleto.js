614c9fbec4494ef5ec465b7e92730c1dc35ffefd
Estefy <estefy.luz.tarasiuk.com>
2023-06-06 21:52:00 -0300

    Subo ejercicios del practico 6/6

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,7 @@
+
+let nombre;
+nombre = "Estefania Luz";
+let apellido = "Tarasiuk";
+let edad  = 34;
+
+console.log("Hola " + nombre + " " + apellido + ". Tienes " + edad + " años.");
---
+++ practico1/miNombreObjecto.js
@@ -0,0 +1,3 @@
+let persona = {nombre: "Estefania Luz" , apellido: "Tarasiuk" , edad: 34};
+
+console.log("Mi objeto persona es el siguiente: " , JSON.stringify(persona));
