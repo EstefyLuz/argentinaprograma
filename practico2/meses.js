9a5e9c8cc475624882ce5fafbf330e2e8bb2da7b
Estefy <estefy.luz.tarasiuk.com>
2023-06-17 10:31:45 -0300

    Ejercicio modificado

--- practico2/meses.js
+++ practico2/meses.js
@@ -2,57 +2,59 @@ const readlineSync = require('readline-sync');
 
 let meses = ["enero", "febrero","marzo", "abril","mayo", "junio", "julio", "agosto","septiembre", "octubre","noviembre", "diciembre"];
 
-let dias30 = 30
-let dias31 = 31
-let dias28 = 28
+let cantidadDias = [28,30,31];
+
 const mes = readlineSync.questionInt('Ingrese un numero de mes: ');
 
 switch (mes) {
     case 1:
-        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , dias31);
+        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , cantidadDias[2]);
         break;
 
     case 2:
-        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , dias28);
+        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , cantidadDias[0]);
         break;
 
     case 3:
-        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , dias31);
+        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , cantidadDias[2]);
         break;
         
     case 4:
-        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , dias30);
+        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , cantidadDias[1]);
         break;
 
     case 5:
-        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , dias31);
+        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , cantidadDias[2]);
         break;
 
     case 6:
-        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , dias30);
+        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , cantidadDias[1]);
         break;
             
     case 7:
-        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , dias31);
+        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , cantidadDias[2]);
         break;
     
     case 8:
-        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , dias31);
+        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , cantidadDias[2]);
         break;
 
     case 9:
-        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , dias30);
+        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , cantidadDias[1]);
         break;
         
     case 10:
-        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , dias31);
+        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , cantidadDias[2]);
         break;
 
     case 11:
-        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , dias30);
+        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , cantidadDias[1]);
         break;
         
     case 12:
-        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , dias31);
+        console.log("La cantidad de dias del mes de %s es %i ", meses[mes-1] , cantidadDias[2]);
         break;
+
+    default:
+        console.log("El numero que ingresaste no corresponde a un mes");
 }
